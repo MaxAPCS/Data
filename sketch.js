dc5ef4a31091119f3d46d4db2f062db7d36c0ceb
Max L <MALIU2@s.sfusd.edu>
2024-02-01 10:06:02 -0800

    first

--- sketch.js
+++ sketch.js
@@ -1,36 +1,58 @@
-function setup() {
-  createCanvas(400, 400);
-  background(0)
-  noStroke()
+let billionaires;
+
+async function setup() {
+  billionaires = (await fetch("billionaires.csv").then(r => r.text())).slice(1, -2).split('"\n"').map(row => row.split('","')).map((record, i, fulllist) => Object.fromEntries(fulllist[0].map((label , i) => [label, record[i]]))).slice(1);
+  createCanvas(800, 400);
+  noLoop()
 }
 
-let h = 0, t = 0;
-let animprog = 0;
 function draw() {
-  fill(0)
-  rect(0, 0, width, 100)
-  fill(0xfff)
-  textAlign(LEFT,TOP)
-  textSize(20)
-  text(`Heads: ${h} (${100 * h / (h+t)}%)`, 0, 0)
-  translate(width/2, height/2);
-  if (animprog > 0) {
-    fill(0)
-    rect(-50, -50, 100, 100);
-    fill(150, 130, 0)
-    ellipse(0, 0, 50, animprog%100-50)
-    if (animprog === 1) {
-      let heads = Math.random() < 0.5;
-      fill(0)
-      textAlign(CENTER,CENTER)
-      textSize(40)
-      text(heads ? "H" : "T", 0, 0)
-      if (heads) h++; else t++;
-    }
-    animprog--
+  if (!billionaires) return redraw();
+  translate(100, height-100);
+  scale(1, -1)
+  background(0);
+  noStroke()
+  for (let record of billionaires) {
+	let x = parseInt(record["demographics.age"])
+	if (x <= 0) continue;
+	let y = parseFloat(record["wealth.worth in billions"]);
+	if (record["demographics.gender"] !== "male") {fill(0, 150, 0)} else {fill(0xfff)}
+  	circle((x-10) * 7, y * 3, 5);
   }
-}
+  noFill()
+  stroke(0xfff);
+  line(-10, -10, width, -10)
+  line(-10, -10, -10, 300)
+  scale(1, -1)
 
-function keyPressed() {
-  animprog = (animprog > 1) ? 1 : 150;
+  textSize(18)
+  textAlign(CENTER, TOP)
+  for (let x = 20; x <= 100; x += 20) {
+    noFill()
+    stroke(0xfff);
+    line((x-10) * 7, 15, (x-10) * 7, 5);
+    fill(0xfff);
+    noStroke()
+    text(x, (x-10) * 7, 18)
+  }
+  textSize(30)
+  text("Age", width/2-50, 40)
+
+  textSize(18)
+  textAlign(RIGHT, CENTER)
+  for (let y = 10; y <= 90; y += 10) {
+    noFill()
+    stroke(0xfff);
+    line(-5, -y*3, -15, -y*3);
+    fill(0xfff);
+    noStroke()
+    text(y, -18, -y*3)
+  }
+  textSize(30)
+  textAlign(CENTER, BOTTOM)
+  push()
+  translate(-40, -height/2+50)
+  rotate(3*PI/2)
+  text("Net Worth (B)", 0, 0)
+  pop()
 }
